75c2d311265056f8daa17731f90c79a6129f9d19
Victor <u273342@cheaz.ru>
2020-12-15 08:11:36 +0300

    1. Add core class library project 2. Add console ui project

---
+++ PrinterManager.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrinterManager.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
